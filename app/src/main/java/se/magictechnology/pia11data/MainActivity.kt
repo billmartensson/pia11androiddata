60db7a65a4ee2506c34c280d6b4a4cc8eca8a805
Bill Mårtensson <bill@magictechnology.se>
2023-02-09 13:31:55 +0100

    pia11

--- app/src/main/java/se/magictechnology/pia11data/MainActivity.kt
+++ app/src/main/java/se/magictechnology/pia11data/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
+import se.magictechnology.pia11data.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,7 +16,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        //setContentView(R.layout.activity_main)
+
+        val binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.fancytext = "Banan"
 
         val nameObserver = Observer<String> { newName ->
             findViewById<TextView>(R.id.mainTextview).text = newName
@@ -24,6 +31,8 @@ class MainActivity : AppCompatActivity() {
 
         val numberObserver = Observer<Int> {
             findViewById<TextView>(R.id.mainTextview).text = it.toString()
+
+            binding.fancytext = it.toString()
         }
 
         model.theNumber.observe(this, numberObserver)
@@ -33,7 +42,6 @@ class MainActivity : AppCompatActivity() {
         findViewById<Button>(R.id.mainButton).setOnClickListener {
             model.currentName.value = "Bartil"
         }
-
 
     }
 
